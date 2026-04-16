d6ef6078f7eed16f96d09b9b25e0e7852f588327
madprops <easywest@protonmail.com>
2026-04-16 10:18:00 -0600

    Mods

--- js/main/users.js
+++ js/main/users.js
@@ -260,7 +260,6 @@ App.user_madprops_settings = () => {
   // Tab Box
 
   App.set_setting({setting: `show_tab_box`, value: true})
-  App.set_setting({setting: `tab_box_mode`, value: `special`})
   App.set_setting({setting: `tab_box_size`, value: `normal`})
   App.set_setting({setting: `tab_box_colors_enabled`, value: true})
   App.set_setting({setting: `tab_box_color`, value: `rgba(47, 99, 151, 0.18)`})
